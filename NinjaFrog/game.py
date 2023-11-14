b90d1c82dc40857cd401e0ac565ec36e425a68c9
victorhugobarbosa <victorhugobarbosa10000@gmail.com>
2023-11-14 11:02:26 -0300

    push the files

---
+++ NinjaFrog/game.py
@@ -0,0 +1,81 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+
+#from pygame.sprite import _Group#
+pygame.init()
+
+pygame.display.set_caption("Platformer")
+
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+"""
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+        
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+        
+    def move_right(self, vel):
+        self.x_vel = 
+"""
+
+def get_background(name):
+    image = pygame.image.load(join("assets","Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Purple.png")
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw(window, background, bg_image)
+
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(window)
